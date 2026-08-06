7a446e2cdd4b8333bde5b0352107698923a30637
oraorajajo <kkokk88@naver.com>
2026-08-06 10:07:54 +0900

    Initial commit: Spring Boot project scaffold

---
+++ src/main/java/com/example/mealdangapi/MealdangapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.mealdangapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealdangapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealdangapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mealdangapi/MealdangapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mealdangapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealdangapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
